963b26e1b5a663d2787efc2bafda8eac9a298c89
sherinakuijpers <55694802+sherinakuijpers@users.noreply.github.com>
2020-03-23 10:13:28 +0100

    Add files via upload

--- functions.php
+++ functions.php
@@ -1,87 +1,88 @@
-<?php
-function DBconnect ()
+<?php 
+function dBConnect()
 {
-    $servername = "localhost";                //naam van de server
-    $username = "root";                       //username om in te loggen
-    $password = "";                           //wachtwoord im in te loggen. standaard is deze leeg
-    $dbname = "dc heroes";                    //naam van de databse
-
-    $connection = new mysqli($servername, $username, $password, $dbname) or die ($connection->error); 
-    //verbind met de database of anders geef een error als er geen connectie is
-
-    return $connection;                       //als je de functie aanroept geeft hij de $connection terug
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbName = "dc heroes";
+    //variabellen voor de database connectie
+    $connection = mysqli_connect($servername,$username,$password,$dbName) or die(mysqli_error($connection));
+    //nieuw object om de connectie met de database te maken
+    return $connection;
 }
 
-	function getTeams()
-	{
-		$connection = DBconnect();            //verbind met database
-
-		$teams = array();                     //maak een array aan om de teams in op te slaan
-                                              //maak een query om data uit de database te halen
-		$getTeamsSQL = "SELECT * FROM `teams` ORDER BY `teamName` ASC";
-
-		$resource = mysqli_query($connection, $getTeamsSQL) or die(mysqli_error($connection));
-
-		while($row = mysqli_fetch_assoc($resource))            //zolang er resultaten zijn, zet deze in de array $teams
-		{
-			$teams[] = $row;
-		}
-
-		return $teams;                                        //als de functie word aangeroepen geeft hij de array $teams terug
-	}
-
-	function getCharacters($teamId = false)
-	{
-		$connection = DBconnect();                            //verbind met database
-
-		$characters = array();                                     //maak een array aan om de teams in op te slaan
-                                                              //maak een query om data uit de database te halen
-		$getCharactersSQL = "SELECT * FROM `characters` -- ORDER BY `teamId` ASC--";
-
-		if($teamId)
-		{
-			$getCharactersSQL .= "WHERE teamId = $teamId";
-		}
-
-		$getCharactersSQL .= "ORDER BY `characterName` ASC";
-
-		$resource = mysqli_query($connection, $getCharactersSQL) or die(mysqli_error($connection));
-
-		while($row = mysqli_fetch_assoc($resource))
-		{
-			$characters[] = $row;                              //zolang er resultaten zijn, zet deze in de array $characters
-		}
-
-		return $characters;                                    //als de functie word aangeroepen geeft hij de array $characters terug
-	}
-
-	function GetTeamCharacter($characterId = false, $teamId = true)
-	{
-		$connection = DBconnect();                            //verbind met database
-		$teamCharacter = array();
-
-		if($characterId)
-		{
-			$getTeamCharacterSQL = "SELECT * FROM `characters` JOIN teams ON characters.teamId = teams.teamId WHERE characters.characterId = '$characterId';";
-		}
-		else {
-			$getTeamCharacterSQL = "SELECT * FROM characters JOIN teams ON characters.teamId = teams.teamId WHERE teams.teamId = '$teamId' ORDER BY RAND() LIMIT 1";
-		}
-
-		$resource = mysqli_query($connection, $getTeamCharacterSQL) or die (mysqli_error($connection));
-
-		$teamCharacter = mysqli_fetch_assoc($resource);
-
-		return $teamCharacter;
-	}
-
-function myDump($myVar)             
+function getTeams()                                        
 {
-	echo "<pre>";
-	var_dump($myVar);
-	echo "</pre>";
+    $connection = dBConnect();                                            //verbinding met database
+    
+    $teams = array();                                                     // definieer een lege array om de teams in op te slaan
+
+    $getTeamsSQL = "SELECT * FROM `teams` ORDER BY `teamName` ASC";       //sql om de teamnaam uit de tabel teams te halen
+
+    $resource = mysqli_query($connection, $getTeamsSQL) or die (mysqli_error($connection));    //voer de query uit, als dat niet lukt geef een error
+
+    while($row = mysqli_fetch_assoc($resource))                           //zolang er resultaten zijn, zet deze in de array teams
+    {
+        $teams[] = $row;
+    }
+
+    return $teams;                                                        //definieer de query om de gegevens op te roepen uit de database
+
 }
 
+function getTeamMembers($teamId = false)                             
+{
+    $connection = dBConnect();                                            //verbinding met database
+    
+    $teamMembers = array();                                               // definieer een lege array om de teams in op te slaan
+
+    $getTeamMembersSQL = "SELECT * FROM `characters`";                    //sql om alle informatie uit de database characters te halen
+
+    if($teamId)                                                           //als er een teamIs is ingevuld..
+    {
+        $getTeamMembersSQL .= " WHERE teamID = $teamId ";                 //haal uit database alles met dat teamId waar op geklikt is
+    }
+    $getTeamMembersSQL .= " ORDER BY `characterName` ASC";                //zet de character namen op volgorde
+
+    $resource = mysqli_query($connection, $getTeamMembersSQL) or die (mysqli_error($connection));    //voer de query uit, als dat niet lukt geef een error
+
+    while($row = mysqli_fetch_assoc($resource))                           //zolang er resultaten zijn..
+    {
+        $teamMembers[] = $row;                                            //zet deze in een array
+    }
+
+    return $teamMembers;                                                  //definieer de query om de gegevens op te roepen uit de database
+
+}
+
+function getTeamMember($teamMemberId = false, $teamId = true )
+{
+    $connection = dBConnect();                                             //verbinding met database
+    
+    $teamMember = array();                                                 //definieer een lege array om de teams in op te slaan
+
+    if($teamMemberId)                                                      //als er een teamMemberId is bepaald pak deze query
+    {
+        $getTeamMemberSQL = "SELECT * FROM `characters` JOIN teams ON characters.teamID = teams.teamID WHERE characters.characterId =  '$teamMemberId';";
+    }
+    else                                                                   //anders pak deze query
+    {
+        $getTeamMemberSQL = "SELECT * FROM characters JOIN teams ON characters.teamID = teams.teamID WHERE teams.teamID = '$teamId' ORDER BY RAND() LIMIT 1";  
+    }
+
+    $resource = mysqli_query($connection, $getTeamMemberSQL) or die (mysqli_error($connection));     //voer de query uit, als dat niet lukt geef een error
+
+    $teamMember = mysqli_fetch_assoc($resource);                            //zet de resultaten in de variabele teamMember
+
+    return $teamMember;                                                     //definieer de query om de gegevens op te roepen uit de database
+}
+
+function myDump($myVar)                                                     //functie hoe de resultaten moeten komen te staan
+{
+    echo "<pre>";
+    var_dump($myVar);
+    echo "</pre>";
+}
 ?>
 
 
--- index.php
+++ index.php
@@ -1,28 +1,26 @@
-<?php
-	include("includes/functions.php");                          //inlcude functions from includes/functions.php
-	
-	$teams = getTeams();                                        //stop de functie in de variabele $teams
+<?php 
+include("includes/functions.php");                  //gebruik het bestand functions.php
 
-	$characters = getCharacters();                              //stop de functie in de variabele $characters
+$teams = getTeams();                                 //de functie getTeams word in de variabele $teams gezet
 
-	if(isset($_GET['teamId']))
-	{
-    	$characters = getCharacters($_GET['teamId']);
-	}
-	else
-	{	
-    	$characters = getCharacters();
-	}
+if(isset($_GET['teamId']))                           //als er een teamId is aangeklikt dan word dat teamId in de variabele $teamMembers gezet
+{
+    $teamMembers = getTeamMembers($_GET['teamId']);
+}
+else
+{
+    $teamMembers = getTeamMembers();                 //anders word de functie getTeamMembers in de variabele $teamMembers gezet
+}
 
-	if(isset($_GET['characterId']))
-	{
-    	$character = getCharacters($_GET['characterId']);
-	}
-	else
-	{
-    	$character = getCharacters();
-	}
-?>
+if(isset($_GET['teamMemberId']))                     //als er een teamMemberId is aangeklikt dan word dat teamMemberId in de variabele $teamCharacter gezet
+{
+    $teamCharacter = getTeamMember($_GET['teamMemberId']);
+}
+else
+{ 
+    $teamCharacter = getTeamMember();                //anders word de functie in deze variabele gezet
+}
+?> 
 <!DOCTYPE html>
 <html>
 <head>
@@ -32,81 +30,81 @@
 	<title>DC Heroes</title>
 </head>
 <body>
-	<?php include "includes/header.php"; ?>                         <!-- voeg het bestand header.php toe -->
+<?php include "includes/header.php"; ?>           <!-- voeg het bestand header.php toe -->
+	
 	<div id="main-container">
+
 		<div id="main-left">
-			
-			<nav id= "navigation">
-				<ul>
-					<?php
-					foreach($teams as $key => $team)                //voor elke $team in de tabel $teams..
-					{ 
-						?>
-						<div class= "navblocks">
-						<img src="img/<?php echo $team['teamImage'];?>" height="100" width="75" alt="">
-						<div class= "teamname">                     <!-- echo de naam van elk team --> 
-							 <li><a href="<?php echo $_SERVER['PHP_SELF']; ?>?teamId=<?php echo $team["teamId"]; ?>"><?php echo $team["teamName"]; ?></a></li>     
-						</div>
-					</div>
-						<?php
-					}
-					?>
-				</ul>
-			</nav>
+			<div class= "Teams">
+				<h2>Teams</h2>
+			</div>
+            <ul>  
+                <?php 
+                    foreach($teams as $key => $team)
+                    {
+						?>   
+						<div id= "teams">
+						<div class= "teamimage">
+							<img src="images/<?php echo $team['teamImage'];?>" height="85" width="65" alt="">  <!--laat alle foto's van de teams zien-->
+						</div>         
+                            <ul>
+								<div class= "teamnames">          <!--laat alle teamnamen zien met bijbehorende Id. als er op word geklikt gaat hij naar het team-->
+                                	<li><a href="<?php echo $_SERVER['PHP_SELF']; ?>?teamId=<?php echo $team["teamID"]; ?>"><?php echo $team["teamName"];echo "(5)"; ?></a></li>
+								</div>
+							</ul>    
+					</div>          
+                        <?php
+                    }
+                ?>
+            </ul>
 		</div>
 
 		<div id="main-center">
-		
-			<li><a href="<?php echo $_SERVER['PHP_SELF']; ?>?characterId=<?php echo $teamCharacter["characterId"]; ?>&teamId=<?php echo $teamCharacter["teamId"]; ?>"></a></li>
-
-		
-		
-		
-		<div class= "characterbox">
-			<?php
-				foreach ($characters as $key => $character)                 //voor elke $character in de tabel $characters..
-				{
-					?>
-					<div class= "characters">
-					<div class= "charactername">
-						<?php echo $character["characterName"]; ?>          <!-- echo elke held naam -->
-					</div>
-					<div class= "characterimage">                           <!-- echo elke held image -->
-                    	<img src= "img/<?php echo $character['characterImage'];?>" height= "280" width= "150">
-					</div>    
-					<div class= "characterdescription">
-						<?php echo $character["characterDescription"]; ?>    <!-- echo elke held omschrijving -->
-					</div>    
-					<div class= "charactermoreinfo">
-						<input type= "submit" name= "more info" value= "More info">     <!-- button met meer info -->
-					</div>
-
-				</div>
-					<?php
-				}
-			
+		<?php 
+            foreach($teamMembers as $key => $teamMember)              
+            {
 			?>
+			<div id="teammember">   
+				<div class= "picture">                                           <!--laat alle images zien voor elke teamMember-->
+					<img src="images/<?php echo $teamMember['characterImage'];?>" height="200" width="100" alt="">
+				</div>
+				<div class= "text">
+					<div class= "name">
+						<ul>                                                     <!--laat alle characternames zien met onzichtbare id-->
+                        	<li><a href="<?php echo $_SERVER['PHP_SELF']; ?>?teamMemberId=<?php echo $teamMember["characterId"]; ?>&teamId=<?php echo $teamMember["teamID"]; ?>"><?php echo $teamMember["characterName"];   ?></a></li>
+                        </ul>
+					</div>
+					<div class= "description">
+						<?php echo $teamMember["characterDescription"]; ?>    <!-- echo elke held omschrijving -->
+					</div>  
+				</div>
 			</div>
+			<?php
+            }           
+        ?>          
 		</div>
-			
 
 		<div id="main-right">
-			Right container
-			<?php   
-				echo $character['characterName'];         
-                echo "<br >";
-				echo $character['characterDescription'];
-			?>
-			<div id="review-form">
-                    <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
-                        <textarea name="reviewtext" id="" cols="30" rows="10"></textarea>
-                        <input type="hidden" name="bid" value="<?php echo $character['characterID'];?>">
-                    </form>
-                </div>
+			<div id= "characterright">
+				<div class= "characterimage">                                    <!--laat de image zien van het team waar op geklikt is-->
+					<img src="images/<?php echo $teamMember['teamImageR'];?>" height="500" width="220" alt="">
+				</div>
+				<div class= "character">
+				<div class= "characterimageround">                               <!--laat de image zien van het character waar op geklikt is-->
+					<img class= "img-circle" src="images/<?php echo $teamCharacter['characterImage'];?>" height="150" width="150" alt="">
+				</div>
+				<div class= "nameright">
+					<?php echo $teamCharacter['characterName'];?>                <!--laat de naam van het character zien-->
+				</div>
+				<div class= "descriptionright">
+					<?php echo $teamCharacter['characterDescription'];?>         <!--laat de beschrijving van het character zien-->
+				</div>
+				<div class= "propertiesright">
+					<?php echo $teamCharacter['characterProperties'];?>          <!--laat de eigenschappen van het character zien-->
+				</div>
+				</div>
+			</div>
 		</div>
-		
-
-	</div>
-	<?php include "includes/footer.php"; ?>               <!-- voeg het bestand footer.php toe -->
+		<?php include "includes/footer.php"; ?>
 </body>
 </html>
